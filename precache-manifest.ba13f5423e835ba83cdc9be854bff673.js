a5b8d916006a456c67f81d5c66d5b4c1a4bbf3e4
Armi <armi.avelino@gmail.com>
2019-04-07 16:38:57 -0600

    Updates

---
+++ precache-manifest.ba13f5423e835ba83cdc9be854bff673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5da141729d8820965909",
+    "url": "/React_Clicky_Game/static/js/runtime~main.d936d443.js"
+  },
+  {
+    "revision": "094d00fde986377995d2",
+    "url": "/React_Clicky_Game/static/js/main.ee2ca0ec.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/React_Clicky_Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "094d00fde986377995d2",
+    "url": "/React_Clicky_Game/static/css/main.f4e7663f.chunk.css"
+  },
+  {
+    "revision": "bceda315c58032d649b2c14dd8510522",
+    "url": "/React_Clicky_Game/index.html"
+  }
+];
